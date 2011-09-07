0b209e4176463027fbda9aaa257e201edbde039d
Jonah Williams <jonah@carbonfive.com>
2011-09-07 16:43:29 -0700

    pbx parsing rules

--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -8,62 +8,87 @@ class Parse
 end
 
 module MiniTest::Assertions
-  def must_parse(content)
-    parser = PbxprojParser.new
-    result = parser.parse(content)
-    assert !result, "parse error: #{parser.failure_reason} #{parser.failure_line} #{parser.failure_column}"
+  def must_parse(content)    
+    result = @parser.parse(content)
+    assert result != nil, "parse error: #{@parser.failure_reason} #{@parser.failure_line} #{@parser.failure_column}\nconsumed #{@parser.index}/#{content.length} characters from:\n#{content}"
   end  
 end
  
 describe Parse do
   before do
     Treetop.load 'pbxproj'
+    @parser = PbxprojParser.new
   end
  
   describe "comments" do
-    before do
-      @content = <<END_PBXPROJ_CONTENT
+    it "parses end of line comments" do
+      must_parse <<END_PBXPROJ_CONTENT
 // !$*UTF8*$! /* file header */
+//
 END_PBXPROJ_CONTENT
     end
-    it "parses successfully" do
-      must_parse @content 
+    
+    it "parses c comments" do
+      must_parse <<END_PBXPROJ_CONTENT
+/* comment */
+END_PBXPROJ_CONTENT
+    end
+    
+    it "parses commented value" do
+      must_parse <<END_PBXPROJ_CONTENT
+{value /* comment */ = otherValue /* comment */;}
+END_PBXPROJ_CONTENT
     end
   end
  
-  describe "hashes" do
-    before do
-      @content = <<END_PBXPROJ_CONTENT
-{}; /* empty hash */
-{ /* comment in hash */ };
+  describe "dictionaries" do
+    it "parses an empty dictionary" do
+      must_parse <<END_PBXPROJ_CONTENT
+{} /* empty hash */
+END_PBXPROJ_CONTENT
+    end
 
+    it "parses nested dictionaries" do
+      must_parse <<END_PBXPROJ_CONTENT
 {
   archiveVersion = 1;
   classes = {
+    nestedHash = true;
   };
   objectVersion = 45;
-};
+}
 END_PBXPROJ_CONTENT
     end
-    it "parses successfully" do
-      must_parse @content 
+    
+    it "parses dictionaries with string keys" do
+      must_parse <<END_PBXPROJ_CONTENT
+{
+  "valueWithStringEscapedCharacters[foo=bar]{}--;;()" = 1;
+  "value with some spaces" = 45;
+}
+END_PBXPROJ_CONTENT
     end
   end
   
   describe "arrays" do
-    before do
-      @content = <<END_PBXPROJ_CONTENT
+    it "parses an empty array" do
+      must_parse <<END_PBXPROJ_CONTENT
 {
-  buildConfigurations = (
-	C01FCF4F08A954540054247B /* Debug */,
-	C01FCF5008A954540054247B /* Release */,
-	E22E41D81281F13F006A09EA /* Distribution */,
-);
-};
+  foo = ();
+}
 END_PBXPROJ_CONTENT
     end
-    it "parses successfully" do
-      must_parse @content
+    
+    it "parses an array" do
+      must_parse <<END_PBXPROJ_CONTENT
+{
+  buildConfigurations = (
+    C01FCF4F08A954540054247B /* Debug */,
+    C01FCF5008A954540054247B /* Release */,
+    E22E41D81281F13F006A09EA /* Distribution */,
+  );
+}
+END_PBXPROJ_CONTENT
     end
   end
 end
