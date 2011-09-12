55177b517409a8489acd0eb14fa8cddace2e3ca8
Jonah Williams <jonah@carbonfive.com>
2011-09-12 09:31:36 -0600

    test PbxAssignment

--- spec/pbx_assignment_spec.rb
+++ spec/pbx_assignment_spec.rb
@@ -12,6 +12,21 @@ END_PBXPROJ_CONTENT
     
     project_node = Parser.parse(content)
     project_node.must_be_instance_of Pbxproj::PbxProject
-    @assignment_node = project_node.comments.first
-  end  
+    @assignment_node = project_node.root_dictionary.pbx_elements.first
+    @assignment_node.must_be_instance_of Pbxproj::PbxAssignment
+  end
+  
+  describe "variable" do
+    it "returns the variable updated by the assignment" do
+      @assignment_node.variable.must_be_instance_of Pbxproj::PbxValue
+      @assignment_node.variable.to_s.must_equal "value"
+    end
+  end
+  
+  describe "assigned_value" do
+    it "returns the value given by the assignment" do
+      @assignment_node.assigned_value.must_be_instance_of Pbxproj::PbxValue
+      @assignment_node.assigned_value.to_s.must_equal "otherValue"
+    end
+  end
 end
